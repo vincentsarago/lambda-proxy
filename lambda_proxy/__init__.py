0b80abf37f68846505edbf0bcbf11c8d2eb3f8e7
vincentsarago <vincent.sarago@mapbox.com>
2018-08-07 11:11:31 -0400

    major update

--- lambda_proxy/__init__.py
+++ lambda_proxy/__init__.py
@@ -1,3 +1,3 @@
 # lambda_proxy
 
-__version__ = '0.0.4'
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 
-import os
-
 from setuptools import setup, find_packages
 
 with open('lambda_proxy/__init__.py') as f:
@@ -12,24 +10,34 @@ with open('lambda_proxy/__init__.py') as f:
             break
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open('README.rst') as f:
+    readme = f.read()
+
+
+# Runtime requirements.
+inst_reqs = []
+
+extra_reqs = {
+    'test': ['pytest', 'pytest-cov', 'mock']}
 
 
 setup(name='lambda_proxy',
       version=version,
       description=u"Simple AWS Lambda proxy to handle API Gateway request",
-      long_description=u"Simple AWS Lambda proxy to handle API Gateway request",
-      classifiers=['Programming Language :: Python :: 3.6'],
+      long_description=readme,
+      classifiers=[
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 3.6'
+      ],
       keywords='AWS-Lambda API-Gateway Request Proxy',
       author=u"Vincent Sarago",
-      author_email='vincent@mapbox.com',
+      author_email='vincent.sarago@mapbox.com',
       url='https://github.com/vincentsarago/lambda_proxy',
-      license='MIT',
+      license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[],
-      extras_require={
-          'test': ['pytest', 'pytest-cov', 'codecov'],
-      })
+      install_requires=inst_reqs,
+      extras_require=extra_reqs)
