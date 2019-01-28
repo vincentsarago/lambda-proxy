9a1bb802dbeda246978877c471b76fce3e65a343
vincentsarago <vincent.sarago@gmail.com>
2019-01-27 21:57:17 -0500

    update tox and add pre-commit

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
+"""Setup lambda_proxy."""
 
 from setuptools import setup, find_packages
 
-with open('lambda_proxy/__init__.py') as f:
+with open("lambda_proxy/__init__.py") as f:
     for line in f:
         if line.find("__version__") >= 0:
             version = line.split("=")[1].strip()
@@ -10,36 +11,37 @@ with open('lambda_proxy/__init__.py') as f:
             break
 
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
 
 # Runtime requirements.
 inst_reqs = []
 
-extra_reqs = {
-    'test': ['pytest', 'pytest-cov', 'mock']}
+extra_reqs = {"test": ["pytest", "pytest-cov", "mock"]}
 
 
-setup(name='lambda_proxy',
-      version=version,
-      description=u"Simple AWS Lambda proxy to handle API Gateway request",
-      long_description=readme,
-      classifiers=[
-          'Intended Audience :: Information Technology',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7'
-      ],
-      keywords='AWS-Lambda API-Gateway Request Proxy',
-      author=u"Vincent Sarago",
-      author_email='vincent.sarago@mapbox.com',
-      url='https://github.com/vincentsarago/lambda_proxy',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=inst_reqs,
-      extras_require=extra_reqs)
+setup(
+    name="lambda_proxy",
+    version=version,
+    description=u"Simple AWS Lambda proxy to handle API Gateway request",
+    long_description=readme,
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="AWS-Lambda API-Gateway Request Proxy",
+    author=u"Vincent Sarago",
+    author_email="vincent.sarago@gmail.com",
+    url="https://github.com/vincentsarago/lambda_proxy",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=inst_reqs,
+    extras_require=extra_reqs,
+)
